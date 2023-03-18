1f414ac1973331b70965d79b53f9c097204069fd
trieumv <mangvientrieu@gmail.com>
2023-03-18 16:05:33 +0700

    Init Spring boot project

---
+++ src/test/java/com/mangvientrieu/practiceproject/PracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangvientrieu.practiceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
